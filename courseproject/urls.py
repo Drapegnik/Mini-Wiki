62c7e7b283d83fea163f65ffc8d9cea07e5143f1
Drapegnik <ivan.pazhitnykh@gmail.com>
2016-02-04 15:38:32 +0300

    add main page view, navbar, login and register views

--- courseproject/urls.py
+++ courseproject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from app.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home, name='home'),
     url(r'', include('authenticating.urls')),
 ]
