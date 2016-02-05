3ed60812e180e5d713b55b00e8578589f8861d00
Drapegnik <ivan.pazhitnykh@gmail.com>
2016-02-05 04:49:12 +0300

    change app/views.py

--- app/views.py
+++ app/views.py
@@ -5,14 +5,4 @@ from authenticating.models import Account, Theme
 # Create your views here.
 
 def home(request):
-    if request.user.is_authenticated():
-        themeid = Account.objects.filter(username=request.user.username).values_list('theme')
-        if (themeid):
-            param = {'logined': 1, 'username': request.user.username,
-                     'csslink': Theme.objects.filter(id=themeid).values_list('cssLink')[0][0]}
-        else:
-            param = {'logined': 1, 'username': request.user.username,
-                     'csslink': Theme.objects.filter(id=1).values_list('cssLink')[0][0]}
-    else:
-        param = {'csslink': Theme.objects.filter(id=1).values_list('cssLink')[0][0]}
-    return render(request, 'base.html', param)
+    return render(request, 'base.html')
