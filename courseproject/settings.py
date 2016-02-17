189dc5afd4f3dacd480038e582978d7887a0a02a
drapegnik <ivan.pazhitnykh@gmail.com>
2016-02-17 15:01:10 +0300

    swap search backend from 'elasticsearch' to 'whoosh'

--- courseproject/settings.py
+++ courseproject/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'elasticsearch',
+ #   'elasticsearch',
+    'whoosh',
     'haystack',
     'app',
     'authenticating',
@@ -51,15 +52,20 @@ INSTALLED_APPS = [
     'tagging',
 ]
 
+# HAYSTACK_CONNECTIONS = {
+#     'default': {
+#         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+#         'URL': 'http://127.0.0.1:9200/',
+#         'INDEX_NAME': 'haystack',
+#     },
+# }
+
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'haystack',
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
     },
 }
-
-# HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
 FORCE_LOWERCASE_TAGS = True
 
