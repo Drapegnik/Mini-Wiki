7110d9d7ca28324cec9aca65a82f1eb6114598f9
belet_bitbucket <belet.acc@gmail.com>
2016-02-18 21:00:00 +0300

    remove model Ban

--- authenticating/admin.py
+++ authenticating/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from authenticating.models import Account, Ban
+from authenticating.models import Account
 
 
 # Register your models here.
@@ -8,7 +8,7 @@ from authenticating.models import Account, Ban
 class AccountAdmin(admin.ModelAdmin):
     list_display = (
         'username', 'email', 'location', 'gender', 'about', 'theme', 'language', 'created_at', 'updated_at', 'photo',
-        'id')
+        'id', 'is_active')
     fieldsets = [
         ('Basic', {'fields': ['username', 'email', 'password', 'is_admin']}),
         ('User information', {'fields': ['location', 'gender', 'about', 'theme', 'language', 'photo', 'karma']}),
@@ -16,15 +16,4 @@ class AccountAdmin(admin.ModelAdmin):
     ]
 
 
-class Ban_Admin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ['user']}),
-        (None, {'fields': ['reason']})]
-
-    list_display = ('user', 'reason')
-
-    search_fields = ['user', 'reason']
-
-
-admin.site.register(Ban, Ban_Admin)
 admin.site.register(Account, AccountAdmin)
