4c9d8f75131e09250ac5c7a56174069f688f07ad
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2016-04-12 21:08:52 +0300

    switch in Debug=False

--- courseproject/settings.py
+++ courseproject/settings.py
@@ -25,9 +25,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '$qu71e3&tu8y%dzf7)o=0pkwxxq!cjgss$1buo0=y%d9vwum(d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = (
+    'drapegnik-wiki.herokuapp.com',
     'localhost',
     '127.0.0.1',
 )
